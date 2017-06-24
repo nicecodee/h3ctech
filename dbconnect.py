65563b6a139aac1f999c71ef4cfe341001847840
stone <freefor5@gmail.com>
2017-06-24 12:22:49 +0800

    Delete dbconnect.py

--- dbconnect.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: UTF-8 -*-
-import MySQLdb
-
-def connection():
-	conn = MySQLdb.connect(host="localhost", user="root", passwd="jxlgood", db="h3ctech",charset="utf8")
-	
-	
-	c = conn.cursor()
-	return c, conn
